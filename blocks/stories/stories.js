0f91e70f1e6196a949b1e3d8bd04fb1a539714c2
Mihai Indreias <mindreias@adobe.com>
2024-07-08 11:50:52 +0200

    stories breakpoints

--- blocks/stories/stories.js
+++ blocks/stories/stories.js
@@ -1,5 +1,11 @@
 export default function decorate(block) {
 
+    const breakpoints ={
+        mobile: 500,
+        tablet: 800,
+        desktop: 1200
+    };
+
     [...block.children].forEach(elem => elem.replaceWith(elem.firstElementChild));
     [...block.children]
     .filter(elem => !!elem && elem.children.length == 3)
@@ -16,13 +22,37 @@ export default function decorate(block) {
             elem.classList.add('stories-card');
         }
     );
+    window.addEventListener('resize', () => {
+        const maxElementsPerView = {
+            mobile: 3,
+            tablet: 2,
+            desktop: 3
+        };
+        const currentBreakpoint = Object.keys(breakpoints).find(key => window.innerWidth < breakpoints[key]);
+        const maxElements = maxElementsPerView[currentBreakpoint];
 
-    if(block.children.length > 3){
-        const viewMore = document.createElement('button');
-        viewMore.classList.add('stories-view-more');
-        viewMore.textContent = 'View More';
-        block.append(viewMore);
-    }
+        if(block.children.length > maxElements){
+            [...block.children].forEach((elem,index) => {
+                if(index >= maxElements)
+                    elem.style.display = 'none';
+                else
+                    elem.style.display = '';
+            });
+
+            if(block.nextSibling && block.nextSibling.classList.contains('stories-view-more'))
+                return;
+            console.log('created');
+            const viewMore = document.createElement('button');
+            viewMore.classList.add('stories-view-more');
+            viewMore.textContent = 'View More';
+            if(block.nextSibling)
+                block.parentNode.insertBefore(viewMore,block.nextSibling);
+            else
+                block.parentNode.appendChild(viewMore);
+            
+        }
+    });
+    
     return block;
   }
   
