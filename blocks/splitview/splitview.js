6ffc01c1ff2c3e1f3334c38a4bbd532f7c574e61
Mihai Indreias <mindreias@adobe.com>
2024-07-11 11:53:27 +0200

    split view mobile first approach

--- blocks/splitview/splitview.js
+++ blocks/splitview/splitview.js
@@ -1,16 +1,18 @@
-/**
- *
- * @param {Element} block
- */
-export default function decorate(block) {
+function decorateChildren(block, width, setWidth) {
 	const children = [...block.children];
+
 	children.forEach((elem) => {
 		const [left, right, blockProps] = elem.querySelectorAll(':scope > div');
-		const firstWidth = parseInt(blockProps.firstElementChild.textContent);
-
-		left.style.width = `${firstWidth}%`;
-		right.style.width = `${100 - firstWidth}%`;
-
+		if (setWidth) {
+			left.style.width = `${width}%`;
+			right.style.width = `${100 - width}%`;
+			right.style.minHeight = left.style.minHeight = `100%`;
+			right.style.height = left.style.height = `unset`;
+		} else {
+			left.style.height = left.style.minHeight = `${width}%`;
+			right.style.height = right.style.minHeight = `${100 - width}%`;
+			left.style.width = right.style.width = `100%`;
+		}
 		blockProps.style.display = 'none';
 
 		if (block.classList.contains('article')) {
@@ -19,5 +21,27 @@ export default function decorate(block) {
 			description.classList.add('description');
 		}
 	});
+}
+/**
+ *
+ * @param {Element} block
+ */
+export default function decorate(block) {
+	const classes = [...block.classList.values()];
+	const isNotMobile = `(width > 500px)`;
+	const firstWidth = parseInt(
+		classes.find((x) => x.startsWith('width-')).split('-')[1],
+		10
+	);
+	decorateChildren(block, firstWidth, window.matchMedia(isNotMobile).matches);
+
+	window.matchMedia(isNotMobile).addEventListener('change', () => {
+		decorateChildren(
+			block,
+			firstWidth,
+			window.matchMedia(isNotMobile).matches
+		);
+	});
+
 	console.log(children);
 }
